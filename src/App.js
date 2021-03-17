061e0d00559734b32625bef111564a75c3ece688
Prateek Rawat <prateekrawat33@gmail.com>
2021-03-17 23:37:31 +0530

    Added pageviews to app.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import Dashboard from './components/Dashboard/Dashboard'
 import error from './components/404/error'
 import { AuthProvider } from './components/Auth'
 import ReactGA from 'react-ga'
-import { withRouter } from 'react-router-dom'
 
 const App = () => {
   useEffect(() => {
@@ -33,4 +32,4 @@ const App = () => {
   )
 }
 
-export default withRouter(App);
+export default App;
